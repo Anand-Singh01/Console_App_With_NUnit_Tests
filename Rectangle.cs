b7fc93a3f37ca5ebfb353699ee20b81344020e2c
Anandpravesh Singh <Asingh4337@conestogac.on.ca>
2024-03-13 15:15:22 -0400

    Completed Rectangle Class

---
+++ Rectangle.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RectangleAssignment2
+{
+    public class Rectangle
+    {
+        private int _length;
+        private int _width;
+
+        //Default Constructor
+        public Rectangle()
+        {
+            _length = 1;
+            _width = 1;
+        }
+
+        //Non default constructor
+        public Rectangle(int length, int width)
+        {
+            _length = length;
+            _width = width;
+        }
+
+        public int GetLength()
+        {
+            return _length;
+        }
+
+        public void SetLength(int length)
+        {
+            _length = length;
+        }
+
+        public int GetWidth()
+        {
+            return _width;
+        }
+
+        public void SetWidth(int width)
+        {
+            _width = width;
+        }
+
+        public int GetPerimeter()
+        {
+            return 2 * (_length + _width);
+        }
+
+        public int GetArea()
+        {
+            return _length * _width;
+        }
+    }
+}
